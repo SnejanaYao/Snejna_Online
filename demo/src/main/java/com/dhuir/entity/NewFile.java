53a6d845727b71f4554bd65a385e1887f31ed9f4
karolina <karolina@karolina_PC>
2018-03-28 15:15:05 +0800

    第一次提交github项目到网上仓库

---
+++ demo/src/main/java/com/dhuir/entity/NewFile.java
@@ -0,0 +1,18 @@
+package com.dhuir.entity;
+
+import javax.swing.event.ListDataEvent;
+
+public class NewFile extends ListDataEvent {
+
+	public NewFile(Object source, int type, int index0, int index1) {
+		
+		super(source, type, index0, index1);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public static void main(String[] args) {
+		System.err.println("12121212311213213");
+	}
+	
+}
